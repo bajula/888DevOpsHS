d0e4fc467b634a5752a976c2fe548326a530e592
Alex Mitrus <alex@bnetzone.ro>
2020-05-05 16:27:22 +0300

    Added docker install

--- install.sh
+++ install.sh
@@ -36,3 +36,17 @@ EOF
 ln -s /etc/nginx/sites-available/venus.cloud /etc/nginx/sites-enabled/
 nginx -t 
 systemctl restart nginx
+## install docker 
+apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+## add the key 
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+## add repository 
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io 
+## check docker 
+wait 10
+docker run hello-world
